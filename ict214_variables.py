0778160fec089ea4a95e40fa0d531fe2d0a92b1c
mubiana2002 <mubian2002sep@gmail.com>
2025-12-25 10:32:15 -0800

    Add files via upload

---
+++ ict214_variables.py
@@ -0,0 +1,10 @@
+name=input("enter your name: ")
+age=int(input("enter your age"))
+gpa=float(input("enter your gpa"))
+student=input("are you a student(yes/no): ").lower()=='yes'
+
+print(f"NAME: {name} ")
+print(f"AGE: {age} ")
+print(f"GPA: {gpa} ")
+print(f"STUDENT: {student} ")
+
